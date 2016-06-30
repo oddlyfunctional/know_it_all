823589f2e2a3088314fc986e07abebbdace8ae26
mrodrigues <mrodrigues.uff@gmail.com>
2016-06-30 16:08:15 -0300

    Fix extending KnowItAll::Base on more than one subclass

--- lib/know_it_all/base.rb
+++ lib/know_it_all/base.rb
@@ -1,12 +1,15 @@
 module KnowItAll
   class Base
+    def self.validations
+      @validations ||= {}
+    end
+
     def self.assert(method_name, message)
-      @@validations ||= {}
-      @@validations[method_name] = message
+      validations[method_name] = message
     end
 
     def errors
-      @@validations.each
+      self.class.validations.each
         .select { |method_name, _| !self.send(method_name) }
         .map { |_, message| message }
     end
--- test/know_it_all/base_test.rb
+++ test/know_it_all/base_test.rb
@@ -2,18 +2,7 @@ require 'test_helper'
 
 describe KnowItAll::Base do
   describe ".assert" do
-    it "adds the message to the errors set when failed" do
-      expect(policy.errors).must_equal ["Name is missing"]
-    end
-
-    it "doesn't add any message if the validation succeeded" do
-      policy.name = "Something"
-      expect(policy.errors).must_equal []
-    end
-  end
-
-  def mock_policy
-    Class.new(KnowItAll::Base) do
+    class MockPolicy < KnowItAll::Base
       attr_accessor :name
 
       assert :name_present?, "Name is missing"
@@ -22,9 +11,36 @@ describe KnowItAll::Base do
         name && !name.empty?
       end
     end
-  end
 
-  def policy
-    @policy ||= mock_policy.new
+    it "adds the message to the errors set when failed" do
+      expect(MockPolicy.new.errors).must_equal ["Name is missing"]
+    end
+
+    it "doesn't add any message if the validation succeeded" do
+      policy = MockPolicy.new
+      policy.name = "Something"
+      expect(policy.errors).must_equal []
+    end
+
+    describe "more than one class extending KnowItAll::Base" do
+      it "doesn't fail due to expecting `name_present?` method" do
+        class AnotherMockPolicy < KnowItAll::Base
+          attr_accessor :title
+
+          assert :title_present?, "Title is missing"
+
+          def title_present?
+            title && !title.empty?
+          end
+        end
+        expect { AnotherMockPolicy.new }.wont_raise
+
+        policy = AnotherMockPolicy.new
+        expect(policy.errors).must_equal ["Title is missing"]
+
+        policy.title = "Something"
+        expect(policy.errors).must_equal []
+      end
+    end
   end
 end
